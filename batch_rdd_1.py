a45b5ec84fec23a6ed9fc1727d71413ab94617a4
Mokshitha4 <mokshitha.mandadi1443@gmail.com>
2025-05-08 02:07:44 -0400

    changes to handle aggregations

--- batch_rdd_1.py
+++ batch_rdd_1.py
@@ -1,11 +1,8 @@
-# Batch RDD filtering example
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, max
+spark = SparkSession.builder.appName("MaxPerKey").getOrCreate()
 
-spark = SparkSession.builder.appName("LogFiltering").getOrCreate()
-
-logs_rdd = spark.sparkContext.textFile("path/to/logs.txt")
-error_lines_rdd = logs_rdd.filter(lambda line: "ERROR" in line)      # filter lines containing "ERROR"
-# Suppose log format: timestamp,level,message (CSV)
-error_messages_rdd = error_lines_rdd.map(lambda line: line.split(",")[2])  # extract the 'message' field
-error_lines_rdd.collect()  # collect the filtered lines
+lines = spark.sparkContext.textFile("values.csv")
+pairs = lines.map(lambda line: line.split(",")) \
+             .map(lambda fields: (fields[0], int(fields[1])))
+max_result = pairs.reduceByKey(lambda a, b: max(a, b))
+max_result.collect()
--- stream_output_rdd.py
+++ stream_output_rdd.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, explode, split
+from pyspark.sql.functions import expr, split, col, sum, max, min, explode, lit
 spark = SparkSession.builder.appName('pyspark-kafka-streaming').master(
     'spark://spark-master:7077').config('spark.jars.packages',
     'org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0').config(
@@ -7,8 +7,12 @@ spark = SparkSession.builder.appName('pyspark-kafka-streaming').master(
 df_streamed_raw = spark.readStream.format('kafka').option(
     'kafka.bootstrap.servers', 'kafka:9093').option('subscribe', 'topic_test'
     ).option('startingOffsets', 'earliest').load()
-df_lines = df_streamed_raw.selectExpr('CAST(value AS STRING) as line')
-df_words = df_lines.select(explode(split(df_lines.line, ' ')).alias('word'))
-df_counts = df_words.groupBy('word').count()
-query = df_counts.writeStream.format('console').outputMode('complete').start()
+lines_rdd = df_streamed_raw.selectExpr('CAST(value AS STRING) as line')
+lines_rdd = lines_rdd.select(explode(split(lines_rdd.line, ' ')).alias('word'))
+word_counts_rdd = lines_rdd.withColumn('value', lit(1)).withColumnRenamed(
+    'word', 'key')
+word_counts_rdd = word_counts_rdd.groupBy('key').agg(sum('value').alias(
+    'aggregated'))
+query = word_counts_rdd.writeStream.format('console').outputMode('complete'
+    ).start()
 query.awaitTermination()
